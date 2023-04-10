2a12caa76f3d0d722157426302761dc0526fa9c8
mhajisaid <107590294+mhajisaid@users.noreply.github.com>
2023-04-10 03:19:05 +0100

    python code first commit

---
+++ Login_2FA/OSS.py
@@ -0,0 +1,107 @@
+#importing random libarary to give us random number
+import random 
+
+#defining the range for our one time password number
+otp = random.randrange(1000, 10000) 
+
+#create register function to define how user registers new account
+def Register():
+
+#while loop to bring us back incase password dont match with confirm password
+ while True: 
+
+    #username variable so user can input name
+    username = input('> Enter username: ')
+
+    #password variable so user can input name
+    password = input('> Enter Password: ')
+
+    #confirm password variable so user can confirm password
+    confirm_password = input('> Confirm Password: ')
+
+    #if statement to check if password is same as confirm password
+    if password == confirm_password:
+        #open pass.txt in appen mode and store user username and password
+        password_file = open('pass.txt', 'a')
+        password_file.write(f'{username} --> {confirm_password}' "\n")
+        password_file.close()
+
+        #output to tell us if the user created the account successfuly
+        print('> Account Created Successfully!')
+        break
+        #else if statment if the password dont match up with confirm password
+    elif password != confirm_password:
+
+
+        # output if the both passwords dont match 
+        print('> Error, Passwords do not match')
+        continue
+
+#create Login function to define how user login after the account is created
+def Login():
+    while True:
+        #username object so user can input name
+        username = input('> Enter username: ')
+
+        #password object so user can input name
+        password = input('> Enter Password: ')
+
+        #open pass.txt in read mode and to read username and password
+        with open('pass.txt', 'r') as password_file:
+            for line in password_file:
+                line = line.strip()
+                # Split each line into separate username and password strings
+                line_parts = line.split(' --> ')
+                file_username = line_parts[0]
+                file_password = line_parts[1]
+
+                # Check if the username and password match
+                if username == file_username and password == file_password:
+                    #output Welcome username 
+                    print("\nWelcome !" + username )  
+                    return
+            #if the username or password incorrect output invalid username or paaword. Try again    
+            print('> Invalid username or password. Try again!')
+
+
+print('>Enter signup to register')
+print('> Enter login to login')
+
+while True:
+    # input function to ask user if they want to signup or login
+    signup_or_login = input('> Do you want to signup or login: ')
+
+    #if user input signup then call up the register function
+    if signup_or_login == 'signup':
+        Register()
+    #else if user input login then call up the login function
+    elif signup_or_login == 'login':
+        Login() 
+        while True: 
+            # if the user completes the login steps successfully then open otp.txt or create file if the file is not there
+            otp_file = open('otp.txt', 'a')
+            # write data onto otp file 
+            otp_file.write(f'One Time Password: {otp}' "\n")
+            #close otp file 
+            otp_file.close()
+
+            #create code veriable to input otp code 
+            code = input('> Please Enter Your One Time Password:')
+            #open otp file in read mode 
+            otp_file = open('otp.txt','r').read()
+            # if statment to check if the code is in the file 
+            if code in otp_file:
+                #output access granted if the code entred is correct 
+                print('> Access Granted!!!')
+                break
+            else:
+                #output access denied if the code entred is incorrect 
+                print('> Access Denied!!!')
+            continue     
+        
+        
+    else:
+        # error output to tell user to entier signup or login
+        print('> Error, enter a valid input: (signup or login)')
+        break
+    continue
